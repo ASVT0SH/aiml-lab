68aa4ca6417c08adad0c57ade403a6264f6a6cd4
Asutosh  Chamappaji Urs <48209722+ASVT0SH@users.noreply.github.com>
2024-01-09 06:49:35 +0530

    added comments

--- 9_regression.py
+++ 9_regression.py
@@ -6,7 +6,7 @@ def local_regression(x_target, features, labels, bandwidth):
     features_expanded = [[1, i] for i in features]
     features_expanded = np.asarray(features_expanded)
     x_weights = (features_expanded.T) * np.exp(np.sum((features_expanded - x_target) ** 2, axis=1) / (-2 * bandwidth))
-    regression_coefficients = np.linalg.pinv(x_weights @ features_expanded) @ x_weights @ labels @ x_target 
+    regression_coefficients = np.linalg.pinv(x_weights @ features_expanded) @ x_weights @ labels @ x_target # @ symbol used for matrix multiplication
     return regression_coefficients 
 
 def plot_local_regression(bandwidth):
